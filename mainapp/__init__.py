987e714349687b3dc22b104c3325660cd8a3ba4a
Fabio Costa <72fcosta@gmail.com>
2017-07-14 06:48:57 -0300

    Commit

--- mainapp/__init__.py
+++ mainapp/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 from flask_wtf import CSRFProtect
 from flask_mail import Mail
 from config import DevelopmentConfig
